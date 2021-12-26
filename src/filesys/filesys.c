44f33cde22ecdb65911ae72ad403b95f72abca86
jinght <jinght@shanghaitech.edu.cn>
2021-12-26 18:57:02 +0900

    Remove unwanted char

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -37,7 +37,7 @@ filesys_done (void)
 {
   free_map_close ();
 }
-
+
 /* Creates a file named NAME with the given INITIAL_SIZE.
    Returns true if successful, false otherwise.
    Fails if a file named NAME already exists,
